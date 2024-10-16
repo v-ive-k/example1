64c9c68759e8cd4526db0d891a78fd4ff9d4feff
vivek kovi <vivekkovi93@gmail.com>
2024-10-16 16:02:49 -0500

    added mvn files

--- src/main/java/sample/HelloWorld.java
+++ src/main/java/sample/HelloWorld.java
@@ -3,7 +3,7 @@ package sample;
 /**
  * Hello world!
  */
-public class App {
+public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
